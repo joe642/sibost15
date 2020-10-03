7c9c7ae6395653f7c1540a95c54712a0bb21c624
Siya Mchunu <macingwan@gmail.com>
2020-10-03 09:25:19 +0200

    GraphQL and Flask integration

--- analytics/code/api.py
+++ analytics/code/api.py
@@ -1,4 +1,6 @@
-from ariadne import gql, ObjectType, make_executable_schema, fallback_resolvers
+from flask import Flask, request, jsonify
+from ariadne.constants import PLAYGROUND_HTML
+from ariadne import gql, ObjectType, graphql_sync,make_executable_schema, fallback_resolvers
 from ariadne.asgi import GraphQL
 
 from dataclasses import dataclass
@@ -8,7 +10,7 @@ import networkx as nx
 
 # For API server use:
 _data_dir = 'analytics/data'
-from .datasets import Datasets
+from datasets import Datasets
 # For Jupyter use:
 # from datasets import Datasets
 # _data_dir = '../../data'
@@ -456,5 +458,37 @@ def resolve_stats(_, info):
     print("returning", obj)
     return obj
 
-_schema = make_executable_schema(sdl, [ _query, fallback_resolvers ])
-app = GraphQL(_schema, debug=True)
+schema = make_executable_schema(sdl, [ _query, fallback_resolvers ])
+
+app = Flask(__name__)
+
+@app.route("/graphql", methods=["GET"])
+def graphql_playground():
+    # On GET request serve GraphQL Playground
+    # You don't need to provide Playground if you don't want to
+    # but keep on mind this will not prohibit clients from
+    # exploring your API using desktop GraphQL Playground app.
+    return PLAYGROUND_HTML, 200
+
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    # GraphQL queries are always sent as POST
+    data = request.get_json()
+
+    # Note: Passing the request to the context is optional.
+    # In Flask, the current request is always accessible as flask.request
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port='80',debug=True)
+    #app.run(debug=True)
