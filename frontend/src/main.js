3ac383baadf4d8d3b05e4dd0c77012651824d62f
Joseph Tomlinson <joseph.tomlinson@clear.bank>
2020-10-03 11:25:25 +0100

    Added apollo graphql client

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,11 +7,13 @@ import "primeflex/primeflex.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 import "primevue/resources/themes/mdc-light-indigo/theme.css";
+import { createProvider } from "./vue-apollo";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount("#app");
