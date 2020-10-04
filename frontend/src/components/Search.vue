f463aa881a19c29a8ba4f5d44f760a02c67a8b98
Joseph Tomlinson <joseph.tomlinson@clear.bank>
2020-10-04 13:42:02 +0100

    Completed search payment form

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -1,80 +1,175 @@
 <template>
-  <ApolloQuery
-    :query="require('../graphql/StaticData.gql')"
-  >
-    <div class="p-grid">
-      <div class="p-col-6 p-offset-3">
-        <h1>Search Payment Route</h1>
-        <div class="p-fluid">
-          <div class="p-field">
-            <label for="lastname">Origin BIC</label>
-            <Dropdown
-              v-model="form.assetCategory"
-              :options="assetCategories"
-              optionLabel="name"
-              placeholder="Search for a BIC"
-            />
-          </div>
-          <div class="p-field">
-            <label for="lastname">Destination BIC</label>
-            <Dropdown
-              v-model="form.assetCategory"
-              :options="assetCategories"
-              optionLabel="name"
-              placeholder="Search for a BIC"
-            />
-          </div>
-          <div class="p-field">
-            <label for="lastname">Asset Category</label>
-            <Dropdown
-              v-model="form.assetCategory"
-              :options="assetCategories"
-              optionLabel="name"
-              placeholder="Select an Asset Category"
-            />
-          </div>
-          <div class="p-field">
-            <label for="amount">Amount</label>
-            <InputText id="amount" type="text" />
-          </div>
-          <Button label="Search" class="p-button-primary" @click="handleSubmit" />
+  <div class="p-grid">
+    <div class="p-col-6 p-offset-3">
+      <h1>Search Payment Route</h1>
+      <div class="p-fluid">
+        <div class="p-field">
+          <label for="lastname">Origin BIC</label>
+          <Dropdown
+            v-model="form.originBic"
+            :options="data.origins"
+            optionLabel="name"
+            optionValue="bic"
+            placeholder="Search for a BIC"
+          >
+            <template #option="slotProps">
+              <span>{{ slotProps.option.bic }} - {{ slotProps.option.name }}</span>
+            </template>
+          </Dropdown>
         </div>
+        <div class="p-field">
+          <label for="lastname">Destination BIC</label>
+          <Dropdown
+            v-model="form.destinationBic"
+            :options="destinationBics"
+            optionLabel="name"
+            optionValue="bic"
+            placeholder="Search for a BIC"
+          >
+            <template #option="slotProps">
+              <span>{{ slotProps.option.bic }} - {{ slotProps.option.name }}</span>
+            </template>
+          </Dropdown>
+        </div>
+        <div class="p-field">
+          <label for="lastname">Asset Category</label>
+          <Dropdown
+            v-model="form.assetCategory"
+            :options="data.assetCategories"
+            placeholder="Select an Asset Category"
+          />
+        </div>
+        <div class="p-field">
+          <label for="amount">Amount</label>
+          <InputNumber
+            v-model="form.amount"
+            id="amount"
+            mode="decimal"
+            :minFractionDigits="2" 
+          />
+        </div>
+        <div class="p-field">
+          <label for="currency">Currency</label>
+          <AutoComplete
+            v-model="form.currency"
+            :suggestions="filteredCurrencies"
+            @complete="searchCurrency($event)"
+            :dropdown="true"
+            field="code"
+          >
+            <template #item="slotProps">
+              <div>{{slotProps.item.code}} - {{slotProps.item.name}}</div>
+            </template>
+          </AutoComplete>
+        </div>
+        <Button label="Search" class="p-button-primary" @click="handleSubmit" />
       </div>
     </div>
-  </ApolloQuery>
+  </div>
 </template>
 
 <script>
+import AutoComplete from "primevue/autocomplete";
 import Button from "primevue/button";
 import Dropdown from "primevue/dropdown";
-import InputText from "primevue/inputtext";
+import InputNumber from "primevue/inputnumber";
+import gql from 'graphql-tag'
+
+import currencies from "../assets/currencies.json"
+
+const GET_STATIC_DATA_QUERY = gql`
+  query {
+    staticData {
+      assetCategories
+      destinations {
+        party {
+          bdp
+          bic
+          name
+          countryCode
+          countryName
+          city
+        }
+        accounts
+      }
+      origins {
+        bdp
+        bic
+        name
+        countryCode
+        countryName
+        city
+      }
+    }
+  }
+`
 
 export default {
-  name: "HelloWorld",
+  name: "Search",
 
   components: {
+    AutoComplete,
     Button,
     Dropdown,
-    InputText
+    InputNumber
   },
 
   props: {
     msg: String
   },
 
+  computed: {
+    destinationBics () {
+      return this.data.destinations.map((destination) => {
+        return destination.party
+      })
+    }
+  },
+
   data() {
     return {
       form: {
-        assetCategory: ""
+        assetCategory: "",
+        originBic: "",
+        amount: 0,
+        destinationBic: "",
+        currency: ""
       },
-      assetCategories: [{ name: "Cash" }]
+      data: {
+        assetCategories: [],
+        origins: [],
+        destinations: []
+      },
+      currencies: [],
+      filteredCurrencies: []
     };
   },
 
   methods: {
     handleSubmit() {
       this.$router.push({ path: "payment-route" });
+    },
+    searchCurrency (event) {
+        setTimeout(() => {
+            if (!event.query.trim().length) {
+                this.filteredCurrencies = [...this.currencies];
+            }
+            else {
+                this.filteredCurrencies = this.currencies.filter((currency) => {
+                    return currency.code.toLowerCase().startsWith(event.query.toLowerCase());
+                });
+            }
+        }, 10);
     }
+  },
+
+  async mounted () {
+      // this.loading = true
+      const queryResponse = await this.$apollo.query({ query: GET_STATIC_DATA_QUERY })
+      this.data = queryResponse.data.staticData
+      // this.loading = false
+
+      this.currencies = Object.values(currencies)
   }
 };
 </script>
